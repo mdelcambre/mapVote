224bfcf2cd1c4437d0d0812ad6287c329292d4f3
Mark Delcambre <mark@delcambre.com>
2016-10-20 19:15:58 -0400

    WIP

--- storage.js
+++ storage.js
@@ -4,37 +4,15 @@
 
 var drafts = {
 	'test': {
-		teams: ['a', 'b', 'c'],
-		maps: {
+		teamNAME: ['a', 'b'],
+		teamIDs: ['team_a_id', 'team_b_id']
+        maps: {
 			'dorado': {img: 'url', avail: true},
 			'spawn': {img: 'url', avail: true},
 			'route66': {img: 'url', avail:true},
 		},
+        ready: [ false, false]
 		time: 45
-	}
-};
-
-
-var teams = {
-	a: {
-		drafter: true,
-		name: "Team A",
-		draftId: 'test',
-		ready: false,
-		bank: 30,
-	},
-	b: {
-		drafter: true,
-		name: "Team B",
-		draftId: 'test',
-		ready: false,
-		bank: 30,
-	},
-	c: {
-		drafter: false,
-		name: "Obs",
-		draftId: 'test',
-		ready: true
 	}
 };
 
@@ -45,22 +23,41 @@ module.exports = {
 	allTeamsReady: allTeamsReady,
 	getPickOrder: getPickOrder,
 	getDraftState: getDraftState,
+    getTeamNames: getTeamNames,
 }
 
 
-function getTeamType(teamId){
-	if (teams[teamId]) {
-		if(teams[teamId].drafter) { return 'drafter'; }
-		else { return 'obs'; }
+function getTeamType(draftID, teamID){
+	if (drafts[draftID]) {
+		if(teamID && drafts.teamIDs.indexOf(teamID) != -1r) {
+            return 'drafter';
+        } else {
+            return 'obs';
+        }
 	} else {
 		return false
 	}
 }
 
+function getTeamNames(draftID, teamID) {
+    try {
+        if (!teamID) return drafts[draftID].teamNAME;
+        if (drafts[draftID].teamIDs[0] == teamID){
+            return drafts[draftID].teamNAME;
+        } else if (drafts[draftID].teamIDs[1] == teamID){
+            return drafts[draftID].teamNAME.reverse();
+        } else {
+            return false;
+        }
+    } catch(err) {
+        return false;
+    }
+}
 
-function setTeamReady(teamId){
+
+function setTeamReady(draftID, teamID){
 	try {
-		teams[teamId].ready = true;
+		drafts[draftID].ready[drafts[draftID].teamIDs.indexOf(teamID)] = true;
 		return true
 	} catch(err) {
 		return false
@@ -68,11 +65,10 @@ function setTeamReady(teamId){
 }
 
 
-function allTeamsReady(teamId){
+function allTeamsReady(draftID){
 	try {
-		var draft = drafts[teams[teamId].draftId];
-		return draft.teams.every(function(e, i, a){
-			return teams[e].ready;
+		return drafts[draftID].ready.every(function(e, i, a){
+			return e;
 		});
 	} catch(err) {
 		return false;
@@ -80,33 +76,28 @@ function allTeamsReady(teamId){
 }
 
 
-function getPickOrder(teamId, first=false){
+function getPicker(draftID){
 	try {
-		var draftId = teams[teamId].draftId;
-		if (first) {
-			return drafts[draftId].teams[0];
-		} else {
-			return drafts[draftId].teams;
-		}
+        var draft = drafts[draftID];
+        var numVotes = Object.values(draft.maps).reduce(function(prev, el) {
+            if (el.avail){
+                return prev + 1
+            } else {
+                return prev
+            }
+        }, 0);
+        return draft.teamIDs[numVotes % 2];
 	} catch(err) {
-		return []
+		return false;
 	}
 }
 
 
-function getDraftState(teamId) {
+function getDraftState(draftID) {
 	try {
-		var draftId = teams[teamId].draftId;
+	    return maps: drafts[draftID].maps
 	} catch {
 		return false;
 	}
-	
-
-
-	
-	return drafts[draftId].maps
-
-
-
 }
 
